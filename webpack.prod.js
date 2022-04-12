4a8a664c04ecaed59943a2cbc7fb305cfabb2922
Monica Panchal <mpanchal9604@gmail.com>
2022-04-12 17:30:13 -0400

    Added webpack config file

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,5 +3,25 @@ module.exports = {
     output: {
         path: __dirname + '/build',
         filename: 'bundle.js'
+    },
+    resolve: {
+        modules: [
+          'node_modules'
+        ],
+        extensions: ['.js', '.jsx', '.ts'],
+        fallback: {
+            "fs": false,
+            "url":false,
+            "util":false,
+            "stream":false,
+            "path":false,
+            "buffer":false,
+            "querystring":false,
+            "http":false,
+            "net":false,
+            "crypto":false,
+            "zlib":false,
+
+        },
     }
 }
