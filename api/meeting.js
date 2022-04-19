4131fb2641cc73a83281f20c4591f5eab98f9e3f
Monica Panchal <mpanchal9604@gmail.com>
2022-04-19 10:49:50 -0400

    Adding presence status logic

--- api/meeting.js
+++ api/meeting.js
@@ -3,6 +3,12 @@ import Lifx from 'lifxjs';
 const devicePrefix = 'zoom_';
 const participantJoinedEvent = 'meeting.participant_joined';
 const participantLeftEvent = 'meeting.participant_left';
+const userPresenseStatusUpdated='user.presence_status_updated';
+const presenceAvailable='Available';
+const presenceMeeting='In_Meeting';
+const presenceAway='Away';
+const presencePhoneCall='On_Phone_Call';
+const presenceDND='Do_Not_Disturb';
 
 export default async (request, response) => {
   const authValid =
@@ -10,17 +16,26 @@ export default async (request, response) => {
     console.log("Request Headers: "+JSON.stringify(request.headers));
     console.log("Request Body: " + JSON.stringify(request.body.event));
   const event = request.body.event;
+  const presenceStatus=request.body.requestBody.payload.object.presenceStatus;
   const participant = request.body.payload.object.participant;
 
   const isParticipantEvent =
     event &&
-    participant &&
-    (event === participantJoinedEvent || event === participantLeftEvent);
+    participant 
 
-  if (!authValid || !isParticipantEvent) {
+  if (!authValid) {
     // just return a 200, we don't want zoom to keep retrying for a non-2xx error;
     console.log("Auth invalid")
     return response.status(200).send({});
+  }
+
+  if(!isParticipantEvent) {
+    console.log("The participant or event from the webhook request body was null")
+  }
+
+  if(event === userPresenseStatusUpdated) {
+    if (presenceStatus===presenceMeeting) console.log("In Meeting") 
+    else if (presenceStatus===presenceAvailable) console.log("User Available");
   }
 
   console.log('Zoom authenticated...');
@@ -28,6 +43,7 @@ export default async (request, response) => {
 
   const isValidParticipant = participant.email === process.env.ZOOM_PARTICIPANT;
   if (!isValidParticipant) {
+    console.log("Participant: " + participant.email);
     return response.status(200).send({});
   }
 
@@ -47,11 +63,13 @@ export default async (request, response) => {
         x.label.startsWith(devicePrefix)
     );
 
-    const turnOn = event === participantJoinedEvent;
+    const turnRed = (presenceStatus === presenceMeeting) || 
+    (presenceStatus === presenceAway) || (presenceStatus === presencePhoneCall) 
+    || (presenceStatus === presenceDND);
 
-    console.log(`${turnOn ? 'Turning red!' : 'Turning green!'}`);
+    console.log(`${turnRed ? 'Turning red!' : 'Turning green!'}`);
     const promises = filtered.map((device) =>
-      lifx.color.light(device.id, { rgb: (turnOn)?'255,0,0':'0,255,0' })
+      lifx.color.light(device.id, { rgb: (turnRed)?'255,0,0':'0,255,0' })
     );
 
     await Promise.all(promises);
